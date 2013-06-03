df170c14514e7ff353a1577a274afca316be7ae7
Matt Copperwaite <mattcopp+git@gmail.com>
2013-06-03 19:45:50 +0100

    Automated database setup script

---
+++ grabber/db_setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+from models.interface import Interface
+import settings
+
+db = Interface(settings.DATABASE_ENGINE, settings.DB_CONNECTION_STRING)
+
+def build_tables():
+    db.connection.setup_database()
+    
+if __name__ == "__main__":
+    build_tables()
