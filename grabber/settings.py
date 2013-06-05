2b6621c0ec278ed6a4d760ed211a420e3294f775
Matt Copperwaite <mattcopp+git@gmail.com>
2013-06-05 21:53:22 +0100

    Reduced threads for testing

--- grabber/settings.py
+++ grabber/settings.py
@@ -5,7 +5,7 @@ DB_CONNECTION_STRING="sqlite:///grabber.db"
 
 MAX_ACCESS_WAIT=30 # minutes
 MIN_ACCESS_WAIT=3 # minutes
-THREADS=5
+THREADS=1
 
 LOGGING_FORMAT="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 LOGGING_LEVEL=logging.INFO
